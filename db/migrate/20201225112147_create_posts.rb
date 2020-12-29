4eff687a726c5cc8832447d8b44fd2af86484084
ryo0705s <inspire.sasaki15@gmail.com>
2020-12-29 22:44:33 +0900

    postテーブルにuser_idカラムを再追加

--- db/migrate/20201225112147_create_posts.rb
+++ db/migrate/20201225112147_create_posts.rb
@@ -2,10 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.text :content
-
       t.timestamps
-
-    add_column :posts, :user_id, :integer
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_25_122920) do
+ActiveRecord::Schema.define(version: 2020_12_28_144432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_12_25_122920) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
